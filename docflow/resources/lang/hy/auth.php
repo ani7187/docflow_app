09b46787f4c51d88796b6311bffcd580a3f1d675
unknown <azizyana02@gmail.com>
2023-12-31 15:28:50 +0400

    register langs

--- docflow/resources/lang/hy/auth.php
+++ docflow/resources/lang/hy/auth.php
@@ -1,10 +1,12 @@
 <?php
 
 return [
+    'name' => 'Անուն',
     'email_address' => 'Էլ. հասցե',
     'password' => 'Գաղտնաբառ',
     'remember_me' => 'Հիշել',
     'login' => 'Մուտք',
     'register' => 'Գրանցվել',
     'forgot_your_password' => 'Մոռացել եք գացտնաբառը',
+    'confirm_password' => 'Հաստատել գաղտնաբառը',
 ];
--- docflow/resources/views/auth/login.blade.php
+++ docflow/resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ trans('auth.login') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="background: #fcfbfb">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -40,10 +40,10 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-4" style="margin-left: 290px;">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="remember">
+                                    <label class="form-check-label" style="margin-left: 0" for="remember">
                                         {{ trans('auth.remember_me') }}
                                     </label>
                                 </div>
@@ -61,14 +61,14 @@
                             <label class="col-md-3"></label>
                             <div class="col-md-3">
                                 @if (Route::has('register'))
-                                    <a class="btn btn-link" href="{{ route('register') }}">
+                                    <a class="btn btn-link" style="padding: 15px 0 0 65px" href="{{ route('register') }}">
                                         {{ trans('auth.register') }}
                                     </a>
                                 @endif
                             </div>
                             <div class="col-md-6">
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link" style="padding: 15px 0 0 0" href="{{ route('password.request') }}">
                                         {{ trans('auth.forgot_your_password') }}
                                     </a>
                                 @endif
@@ -82,99 +82,105 @@
 </div>
 @endsection
 
-
-
-
-{{--
-@section('content')
+{{--@section('content')
     <!-- Section: Design Block -->
-        <!-- Jumbotron -->
-        <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
-                <div class="row gx-lg-5 align-items-center">
-                    <div class="col-lg-6 mb-5 mb-lg-0">
-                        <h1 class="my-5 display-3 fw-bold ls-tight">
-                            The best offer <br />
-                            <span class="text-primary">for your business</span>
-                        </h1>
-                        <p style="color: hsl(217, 10%, 50.8%)">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Eveniet, itaque accusantium odio, soluta, corrupti aliquam
-                            quibusdam tempora at cupiditate quis eum maiores libero
-                            veritatis? Dicta facilis sint aliquid ipsum atque?
-                        </p>
-                    </div>
+    <!-- Jumbotron -->
+    <div class="px-4 py-5 px-md-5 text-center text-lg-start">
+        <div class="row gx-lg-5 align-items-center mt-5">
+            <div class="col-lg-6 mb-5 mb-lg-0">
+                <h1 class="my-5 display-3 fw-bold ls-tight">
+                    The best offer <br/>
+                    <span class="text-primary">for your business</span>
+                </h1>
+                <p style="color: hsl(217, 10%, 50.8%)">
+                    {{ trans('auth.login') }}
+                </p>
+            </div>
 
-                    <div class="col-lg-6 mb-5 mb-lg-0">
-                        <div class="card">
-                            <div class="card-body py-5 px-md-5">
-                                <form method="POST" action="{{ route('login') }}">
-                                    <!-- 2 column grid layout with text inputs for the first and last names -->
-                                    <div class="row">
-                                        <div class="col-md-6 mb-4">
-                                            <div class="form-outline">
-                                                <input type="text" id="form3Example1" class="form-control" />
-                                                <label class="form-label" for="form3Example1">First name</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 mb-4">
-                                            <div class="form-outline">
-                                                <input type="text" id="form3Example2" class="form-control" />
-                                                <label class="form-label" for="form3Example2">Last name</label>
-                                            </div>
-                                        </div>
-                                    </div>
+            <div class="col-lg-6 mb-5 mb-lg-0">
+                <div class="card">
+                    <div class="card-body py-5 px-md-5" style="background: #f9fbfd; border-radius: 10px;">
+                        <form method="POST" action="{{ route('login') }}">
+                            <!-- 2 column grid layout with text inputs for the first and last names -->
+                            <div class="row mb-3">
+                                <label for="email"
+                                       class="col-md-4 col-form-label text-md-end">{{ trans('auth.email_address') }}</label>
 
-                                    <!-- Email input -->
-                                    <div class="form-outline mb-4">
-                                        <input type="email" id="form3Example3" class="form-control" />
-                                        <label class="form-label" for="form3Example3">Email address</label>
-                                    </div>
+                                <div class="col-md-6">
+                                    <input id="email" type="email"
+                                           class="form-control @error('email') {{ trans('error.invalid')  }} @enderror"
+                                           name="email" value="{{ old('email') }}" required autocomplete="email"
+                                           autofocus>
 
-                                    <!-- Password input -->
-                                    <div class="form-outline mb-4">
-                                        <input type="password" id="form3Example4" class="form-control" />
-                                        <label class="form-label" for="form3Example4">Password</label>
-                                    </div>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                                                                <strong>{{ $message }}</strong>
+                                                                            </span>
+                                    @enderror
+                                </div>
+                            </div>
 
-                                    <!-- Checkbox -->
-                                    <div class="form-check d-flex justify-content-center mb-4">
-                                        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
-                                        <label class="form-check-label" for="form2Example33">
-                                            Subscribe to our newsletter
+                            <div class="row mb-3">
+                                <label for="password"
+                                       class="col-md-4 col-form-label text-md-end">{{ trans('auth.password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password"
+                                           class="form-control @error('password') {{ trans('error.invalid') }} @enderror"
+                                           name="password" required autocomplete="current-password">
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                                                            <strong>{{ $message }}</strong>
+                                                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+                            <div class="row mb-3">
+                                <div class="col-md-6 offset-md-4" style="padding-left: 45px;">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember"
+                                               id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="form-check-label m-lg-0" for="remember">
+                                            {{ trans('auth.remember_me') }}
                                         </label>
                                     </div>
-
-                                    <!-- Submit button -->
-                                    <button type="submit" class="btn btn-primary btn-block mb-4">
-                                        Sign up
-                                    </button>
-
-                                    <!-- Register buttons -->
-                                    <div class="text-center">
-                                        <p>or sign up with:</p>
-                                        <button type="button" class="btn btn-link btn-floating mx-1">
-                                            <i class="fab fa-facebook-f"></i>
-                                        </button>
-
-                                        <button type="button" class="btn btn-link btn-floating mx-1">
-                                            <i class="fab fa-google"></i>
-                                        </button>
-
-                                        <button type="button" class="btn btn-link btn-floating mx-1">
-                                            <i class="fab fa-twitter"></i>
-                                        </button>
-
-                                        <button type="button" class="btn btn-link btn-floating mx-1">
-                                            <i class="fab fa-github"></i>
-                                        </button>
-                                    </div>
-                                </form>
+                                </div>
                             </div>
-                        </div>
+
+
+                            <div class="row mb-0">
+                                <div class="col-md-3 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ trans('auth.login') }}
+                                    </button>
+                                </div>
+                                <div class="col-md-3">
+                                    @if (Route::has('register'))
+                                        <a class="btn btn-link" style="padding-left: 0px" href="{{ route('register') }}">
+                                            {{ trans('auth.register') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-md-3"></label>
+                                <div class="col-md-6">
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" style="padding: 10px 0px 0px 54px" href="{{ route('password.request') }}">
+                                            {{ trans('auth.forgot_your_password') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
+            </div>
         </div>
-        <!-- Jumbotron -->
+    </div>
+    <!-- Jumbotron -->
     <!-- Section: Design Block -->
-@endsection
---}}
+@endsection--}}
